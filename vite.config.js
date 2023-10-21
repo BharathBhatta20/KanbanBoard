f5621b9488a79b7b7dca0b7fefee73f8f9eafbbb
BharathBhatta20 <bharathmbhatta11@gmail.com>
2023-10-21 23:10:06 +0530

    hosting on github-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/https://github.com/BharathBhatta20/KanbanBoard.git",
   test: {
     globals: true,
     environment: 'jsdom',
